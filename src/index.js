38a874c78e41115bce29442c927c69f0a4ffee1c
Komenpurity <jepkorirpurity8@gmail.com>
2023-03-27 13:25:33 +0300

    display images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 // write your code here
+
+let ramenMenu = document.querySelector("#ramen-menu");
+getAllRamen();
+formEventListener();
+
+function getAllRamen(){
+    fetch("http://localhost:3000/ramens")
+    .then(response => response.json())
+    .then(data => ramenItems(data))
+}
+
+
+function ramenItems(data){
+    data.forEach(ramen => {
+        renderImage(ramen);
+        console.log(ramen);
+    });
+}
+
+function renderImage(ramen){
+    let img = document.createElement("img")
+    img.src = ramen.image 
+    img.alt = ramen.name 
+    img.dataset.id = ramen.id 
+    ramenMenu.append(img)
+
+    img.addEventListener("click", function(event){
+                getRamen(event.target.dataset.id);
+    })
+}
+
+
+
+function getRamen(ramenId){
+    fetch(`http://localhost:3000/ramens/${ramenId}`)
+    .then(response => response.json())
+    .then(ramen => {
+        renderDetails(ramen)
+    })
+}
